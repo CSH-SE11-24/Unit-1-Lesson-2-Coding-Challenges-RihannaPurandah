fc740cfb9a00bda1d4471bce46300dafb88d8a33
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-09 09:55:07 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// VARIABLES
+// For the navigator (person giving instructions),
+// Define three variables: `name`, `numberOfSiblings`, and `isBilingual`. Determine which should be constants and which may change.
+let name = "Osiris"
+let numberOfSiblings = 6
+let isBilingual = "yes"
+// Assign corresponding values with the most appropriate types for each
+// Print these variables to the console.
+console.log(name)
+console.log(numberOfSiblings)
+console.log(isBilingual)
+
+
+// RE-ASSIGNMENT
+// Create a variable `counter` and initialize it with 0.
+let counter = 0
+// Set it to a larger value
+counter = 250
+// Increment the `counter` by 1 without specifying the number!
+counter++
+// Print the updated value of `counter`.
+console.log(counter)
+
+// DATA TYPES
+// Create variables for various primitive data types: `number`, `string`, `boolean`, and `null`.
+let number = 16
+let string = "Osiris"
+let boolean = true
+let null =
+
+// Print the values and confirm their type using typeof
+
+// Create a variable that has an undefined type 
+let
+// Print the variable and confirm its type
+
+
+
+// EXTRA CREDIT (Google these!)
+// Make an array of siblings/languages that you speak
+let siblings&language = []
+
+
+// Make an object with your name, siblings, and languages
+
+
+
+// Try printing the number of siblings you have using ONLY the object (Hint: look up how to find the length of an array)
+
+
